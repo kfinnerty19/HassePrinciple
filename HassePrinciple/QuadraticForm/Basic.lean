6c943089c3c1c7b3467080c2228ac607570fd406
mariainesdff <mariaines.dff@gmail.com>
2026-05-11 12:55:29 +0200

    add field section

--- HassePrinciple/QuadraticForm/Basic.lean
+++ HassePrinciple/QuadraticForm/Basic.lean
@@ -25,7 +25,9 @@ section Hyperbolic
 
 open Module
 
-variable {R V W : Type*} [CommRing R] [AddCommGroup V] [Module R V] [AddCommGroup W] [Module R W]
+section CommRing
+
+variable {R V : Type*} [CommRing R] [AddCommGroup V] [Module R V]
 
 /-- A quadratic form is hyperbolic if it is equivalent to the form X^2 - Y^2. -/
 def IsHyperbolic (Q : QuadraticForm R V) : Prop :=
@@ -66,21 +68,29 @@ lemma represents_zero_iff_isotropic {Q : QuadraticForm R V} :
     Q.represents 0 ↔ Q.Isotropic := by
   sorry
 
+end CommRing
+
+section Field
+
+variable {K V W : Type*} [Field K] [AddCommGroup V] [Module K V] [AddCommGroup W] [Module K W]
+
 -- Condition (ii) seems annoying to state, can we avoid it?
-lemma represents_iff_sub_isotropic [Module.Free R V] {Q : QuadraticForm R V} (hQ : Q.Isotropic)
-    (hQ' : Q.Nondegenerate) (r : Rˣ) :
+lemma represents_iff_sub_isotropic {Q : QuadraticForm K V} (hQ : Q.Isotropic)
+    (hQ' : Q.Nondegenerate) (r : Kˣ) :
     Q.represents r ↔
-      (Q.prod (QuadraticMap.weightedSumSquares R ![-r])).Isotropic := sorry
+      (Q.prod (QuadraticMap.weightedSumSquares K ![-r])).Isotropic := sorry
 
-lemma prod_isotropic_iff [Module.Free R V] [Module.Free R W] {Q : QuadraticForm R V}
-    (hQ : Q.Nondegenerate) {Q' : QuadraticForm R W} (hQ' : Q'.Nondegenerate) :
-    (Q.prod Q').Isotropic ↔ ∃ r : Rˣ, Q.represents r ∧ Q'.represents r := sorry
+lemma prod_isotropic_iff {Q : QuadraticForm K V} (hQ : Q.Nondegenerate) {Q' : QuadraticForm K W}
+    (hQ' : Q'.Nondegenerate) :
+    (Q.prod Q').Isotropic ↔ ∃ r : Kˣ, Q.represents r ∧ Q'.represents r := sorry
 
-lemma prod_isotropic_iff' [Module.Free R V] [Module.Free R W] {Q : QuadraticForm R V}
-    (hQ : Q.Nondegenerate) {Q' : QuadraticForm R W} (hQ' : Q'.Nondegenerate) :
-    (Q.prod Q').Isotropic ↔ ∃ r : Rˣ,
-      (Q.prod (QuadraticMap.weightedSumSquares R ![-r])).Isotropic ∧
-      (Q'.prod (QuadraticMap.weightedSumSquares R ![-r])).Isotropic := sorry
+lemma prod_isotropic_iff' {Q : QuadraticForm K V} (hQ : Q.Nondegenerate) {Q' : QuadraticForm K W}
+    (hQ' : Q'.Nondegenerate) :
+    (Q.prod Q').Isotropic ↔ ∃ r : Kˣ,
+      (Q.prod (QuadraticMap.weightedSumSquares K ![-r])).Isotropic ∧
+      (Q'.prod (QuadraticMap.weightedSumSquares K ![-r])).Isotropic := sorry
+
+end Field
 
 end Hyperbolic
 
